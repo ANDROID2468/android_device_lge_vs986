fd2b92a733346c716ac69939f9a28efe42c3bfd1
ANDROID2468 <39718118+ANDROID2468@users.noreply.github.com>
2019-06-15 16:11:33 -0500

    Add files via upload

---
+++ lineage.mk
@@ -0,0 +1,15 @@
+$(call inherit-product, device/lge/h811/full_h811.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Overlays (inherit after vendor/cm to ensure we override it)
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
+PRODUCT_NAME := lineage_h811
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_DEVICE="g4" \
+    PRODUCT_NAME="g4__tmo_us" \
+    BUILD_FINGERPRINT="lge/p1__tmo_us/p1:7.0/NRD90U/1717709471d0f:user/release-keys" \
+    PRIVATE_BUILD_DESC="p1__tmo_us-user 7.0 NRD90U 1717709471d0f release-keys"
