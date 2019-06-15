8551681ca85833f5c6303d31bdb8f57672a7202d
ANDROID2468 <39718118+ANDROID2468@users.noreply.github.com>
2019-06-15 16:30:51 -0500

    Update lineage.mk
    from: https://github.com/Suicide-Squirrel/android_device_lge_vs986/blob/lineage-15.1/full_vs986.mk

--- lineage.mk
+++ lineage.mk
@@ -1,15 +1,30 @@
-$(call inherit-product, device/lge/h811/full_h811.mk)
+  
+#
+# Copyright 2015 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Overlays (inherit after vendor/cm to ensure we override it)
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+# Inherit from device
+$(call inherit-product, device/lge/vs986_usu/device.mk)
 
-PRODUCT_NAME := lineage_h811
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_DEVICE="g4" \
-    PRODUCT_NAME="g4__tmo_us" \
-    BUILD_FINGERPRINT="lge/p1__tmo_us/p1:7.0/NRD90U/1717709471d0f:user/release-keys" \
-    PRIVATE_BUILD_DESC="p1__tmo_us-user 7.0 NRD90U 1717709471d0f release-keys"
+# Set those variables here to overwrite the inherited values.
+PRODUCT_DEVICE := vs986_usu
+PRODUCT_NAME := full_vs986
+PRODUCT_BRAND := lge
+PRODUCT_MODEL := LGVS986
+PRODUCT_MANUFACTURER := LGE
