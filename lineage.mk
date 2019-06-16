c1a94bf8b9602c65f88ea1ffa17374819946f3d4
ANDROID2468 <39718118+ANDROID2468@users.noreply.github.com>
2019-06-16 17:13:26 -0500

    fix the stop errors when you do the lunch command
    I have not test if it compiles yet, but I fix the stop errors with the lunch command

--- lineage.mk
+++ lineage.mk
@@ -1,30 +1,16 @@
-  
-#
-# Copyright 2015 The CyanogenMod Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+$(call inherit-product, device/lge/vs986_usu/full_vs986.mk)
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+# Inherit some common stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-# Inherit from device
-$(call inherit-product, device/lge/vs986_usu/device.mk)
+# Overlays (inherit after vendor/lineage to ensure we override it)
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
-# Set those variables here to overwrite the inherited values.
-PRODUCT_DEVICE := vs986_usu
-PRODUCT_NAME := full_vs986
-PRODUCT_BRAND := lge
-PRODUCT_MODEL := LGVS986
-PRODUCT_MANUFACTURER := LGE
+PRODUCT_NAME := lineage_vs986_usu
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_DEVICE="g4" \
+    PRODUCT_NAME="g4__tmo_us" \
+    PRIVATE_BUILD_DESC="p1_vzw-user 6.0 MRA58K 1714511498b1b release-keys"
+
+BUILD_FINGERPRINT:="lge/p1_vzw/p1:6.0/MRA58K/1714511498b1b:user/release-keys"
